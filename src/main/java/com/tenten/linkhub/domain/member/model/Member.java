b58bfaac8b1bc84a452eb3e3dada8f2e6b6ce9c9
JaeyoungAhn <quaki@naver.com>
2023-10-25 17:26:00 +0900

    [Feat]: 멤버 관련 엔티티 추가

--- src/main/java/com/tenten/linkhub/domain/member/model/Member.java
+++ src/main/java/com/tenten/linkhub/domain/member/model/Member.java
@@ -1,4 +1,80 @@
 package com.tenten.linkhub.domain.member.model;
 
+import com.tenten.linkhub.domain.member.model.vo.AboutMe;
+import com.tenten.linkhub.domain.member.model.vo.NewsEmail;
+import com.tenten.linkhub.domain.member.model.vo.Nickname;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "oauth_email", nullable = false)
+    private String OAuthEmail;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Embedded
+    @Column(name = "nickname", nullable = false)
+    private Nickname nickname;
+
+    @Embedded
+    @Column(name = "about_me")
+    private AboutMe aboutMe;
+
+    @Embedded
+    @Column(name = "news_email")
+    private NewsEmail newsEmail;
+
+    @Column(name = "is_email_verified")
+    private boolean isEmailVerified;
+
+    @OneToMany(mappedBy = "member")
+    private Set<ProfileImage> profileImages = new HashSet<>();
+
+    @OneToMany(mappedBy = "following")
+    private Set<Follow> followings = new HashSet<>();
+
+    @OneToMany(mappedBy = "follower")
+    private Set<Follow> followers = new HashSet<>();
+
+    @OneToMany(mappedBy = "member")
+    private Set<FavoriteCategory> favoriteCategories = new HashSet<>();
+
+    @Builder
+    public Member(Long id, String OAuthEmail, Role role, NewsEmail newsEmail, Nickname nickname, AboutMe aboutMe,
+            boolean isEmailVerified) {
+        this.id = id;
+        this.OAuthEmail = OAuthEmail;
+        this.role = role;
+        this.newsEmail = newsEmail;
+        this.nickname = nickname;
+        this.aboutMe = aboutMe;
+        this.isEmailVerified = false;
+    }
+
 }
--- src/main/java/com/tenten/linkhub/domain/member/model/ProfileImage.java
+++ src/main/java/com/tenten/linkhub/domain/member/model/ProfileImage.java
@@ -1,4 +1,41 @@
 package com.tenten.linkhub.domain.member.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "profile_image")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfileImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "path", nullable = false)
+    private String path;
+
+    @Builder
+    public ProfileImage(Long id, Member member, String path) {
+        this.id = id;
+        this.member = member;
+        this.path = path;
+    }
+
 }
