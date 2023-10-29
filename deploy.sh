1db2ac2369437f8ac02558b261a4ca46b2b2357e
JaeyoungAhn <quaki@naver.com>
2023-10-30 02:14:04 +0900

    [Feat]: Green/Blue 스위칭을 위한 스크립트 작성

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+fi
+
+# Pull the latest Docker image
+docker pull $LINKHUB_IMAGE:latest
+
+# Get the current active environment
+ACTIVE_ENV=$(docker exec swagger-test_nginx_1 nginx -T | grep "server web_" | awk -F'_' '{print $2}' | cut -d ':' -f 1)
+
+# Switch between blue and green environments
+if [ "$ACTIVE_ENV" == "green" ]; then
+  # Stop and remove the current blue environment
+  docker-compose stop web_blue
+  docker-compose rm -f web_blue
+
+  # Start the blue environment
+  docker-compose up -d web_blue
+
+  # Switch traffic to the blue environment
+  docker-compose exec -T nginx sh -c "sed 's/web_green/web_blue/' /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.new && \
+  cat /etc/nginx/nginx.conf.new > /etc/nginx/nginx.conf && \
+  rm /etc/nginx/nginx.conf.new"
+  docker-compose exec -T nginx nginx -s reload
+else
+  # Stop and remove the current green environment
+  docker-compose stop web_green
+  docker-compose rm -f web_green
+
+  # Start the green environment
+  docker-compose up -d web_green
+
+  # Switch traffic to the green environment
+  docker-compose exec -T nginx sh -c "sed 's/web_blue/web_green/' /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.new && \
+  cat /etc/nginx/nginx.conf.new > /etc/nginx/nginx.conf && \
+  rm /etc/nginx/nginx.conf.new"
+  docker-compose exec -T nginx nginx -s reload
+fi
