2aca43841ae220394721f014fdd05d22cc5b67e7
KimMinhee <99alsgmlalsl@naver.com>
2023-10-29 14:25:35 +0900

    [Feat] : 댓글의 group -> groupNumber로 수정 - mysql의 키워드임으로 groupNumber로 필드명 수정

--- src/main/java/com/tenten/linkhub/domain/space/model/space/Comment.java
+++ src/main/java/com/tenten/linkhub/domain/space/model/space/Comment.java
@@ -29,7 +29,7 @@ public class Comment {
     private Comment parentComment;
 
     @Column(nullable = false)
-    private Long group;
+    private Long groupNumber;
 
     @Column(nullable = false)
     private String content;
