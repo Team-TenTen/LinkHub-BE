5617ca984700f43ac4d2b8cb7b924b2a4e5a0107
KimMinhee <99alsgmlalsl@naver.com>
2023-10-31 04:36:16 +0900

    [Feat]: AWS SES 사용을 위한 Config 설정

---
+++ src/main/java/com/tenten/linkhub/global/config/AwsSesConfig.java
@@ -0,0 +1,31 @@
+package com.tenten.linkhub.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService(){
+        final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        final AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(basicAWSCredentials);
+
+        return AmazonSimpleEmailServiceClientBuilder
+                .standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("us-east-1")
+                .build();
+    }
+}
