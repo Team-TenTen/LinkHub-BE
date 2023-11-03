f2fe75edb7be57511ad7ca36bf6189398813ea27
JaeyoungAhn <quaki@naver.com>
2023-11-03 17:56:22 +0900

    [Chore]: 누락 부분 수정

--- src/main/java/com/tenten/linkhub/domain/member/repository/MemberJpaRepository.java
+++ src/main/java/com/tenten/linkhub/domain/member/repository/MemberJpaRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
-    @Query("SELECT m FROM Member m JOIN FETCH m.profileImages pi WHERE m.id IN :memberIds AND m.isDeleted = false")
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.profileImages pi WHERE m.id IN :memberIds AND m.isDeleted = false")
     List<Member> findMemberJoinProfileImageByMemberIds(List<Long> memberIds);
 
     Optional<Member> findBySocialIdAndProvider(String socialId, Provider provider);
