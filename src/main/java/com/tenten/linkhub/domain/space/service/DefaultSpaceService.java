4644c6f28d9b5fa32403a1a4feb6da786d207a1b
KimMinhee <99alsgmlalsl@naver.com>
2023-11-07 04:19:20 +0900

    [Feat]: SpaceService 내 Space 조회 및 권한 확인 메소드 추가

--- src/main/java/com/tenten/linkhub/domain/space/service/DefaultSpaceService.java
+++ src/main/java/com/tenten/linkhub/domain/space/service/DefaultSpaceService.java
@@ -2,12 +2,14 @@ package com.tenten.linkhub.domain.space.service;
 
 import com.tenten.linkhub.domain.space.model.space.Space;
 import com.tenten.linkhub.domain.space.repository.space.SpaceRepository;
+import com.tenten.linkhub.domain.space.repository.spacemember.SpaceMemberRepository;
 import com.tenten.linkhub.domain.space.service.dto.space.SpaceCreateRequest;
+import com.tenten.linkhub.domain.space.service.dto.space.SpaceGetDto;
 import com.tenten.linkhub.domain.space.service.dto.space.SpaceWithSpaceImageAndSpaceMemberInfo;
 import com.tenten.linkhub.domain.space.service.dto.space.SpacesFindByQueryRequest;
 import com.tenten.linkhub.domain.space.service.dto.space.SpacesFindByQueryResponses;
 import com.tenten.linkhub.domain.space.service.mapper.SpaceMapper;
-
+import com.tenten.linkhub.global.exception.UnauthorizedAccessException;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,14 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 import static com.tenten.linkhub.domain.space.model.space.Role.OWNER;
 
 @Service
-public class DefaultSpaceService implements SpaceService{
+public class DefaultSpaceService implements SpaceService {
 
     private final SpaceRepository spaceRepository;
     private final SpaceMapper mapper;
+    private final SpaceMemberRepository spaceMemberRepository;
 
-    public DefaultSpaceService(SpaceRepository spaceRepository, SpaceMapper mapper) {
+    public DefaultSpaceService(SpaceRepository spaceRepository, SpaceMapper mapper, SpaceMemberRepository spaceMemberRepository) {
         this.spaceRepository = spaceRepository;
         this.mapper = mapper;
+        this.spaceMemberRepository = spaceMemberRepository;
     }
 
     @Override
@@ -55,6 +59,18 @@ public class DefaultSpaceService implements SpaceService{
         Space space = spaceRepository.getSpaceJoinSpaceMemberById(spaceId);
 
         return SpaceWithSpaceImageAndSpaceMemberInfo.from(space);
+    }
+
+    @Override
+    public void checkMemberAddLink(Long memberId, Long spaceId) {
+        if (!spaceMemberRepository.existsAuthorizedSpaceMember(memberId, spaceId)) {
+            throw new UnauthorizedAccessException("링크를 생성할 수 있는 권한이 없습니다.");
+        }
+    }
+
+    @Override
+    public SpaceGetDto getSpace(Long spaceId) {
+        return new SpaceGetDto(spaceRepository.getById(spaceId));
     }
 
 }
--- src/main/java/com/tenten/linkhub/domain/space/service/SpaceService.java
+++ src/main/java/com/tenten/linkhub/domain/space/service/SpaceService.java
@@ -1,6 +1,7 @@
 package com.tenten.linkhub.domain.space.service;
 
 import com.tenten.linkhub.domain.space.service.dto.space.SpaceCreateRequest;
+import com.tenten.linkhub.domain.space.service.dto.space.SpaceGetDto;
 import com.tenten.linkhub.domain.space.service.dto.space.SpaceWithSpaceImageAndSpaceMemberInfo;
 import com.tenten.linkhub.domain.space.service.dto.space.SpacesFindByQueryRequest;
 import com.tenten.linkhub.domain.space.service.dto.space.SpacesFindByQueryResponses;
@@ -11,4 +12,8 @@ public interface SpaceService {
     Long createSpace(SpaceCreateRequest spaceCreateRequest);
 
     SpaceWithSpaceImageAndSpaceMemberInfo getSpaceWithSpaceImageAndSpaceMemberById(Long spaceId);
+
+    void checkMemberAddLink(Long memberId, Long spaceId);
+
+    SpaceGetDto getSpace(Long spaceId);
 }
