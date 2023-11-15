486740ad0a611dbae94c266d0a403516bff182cd
JaeyoungAhn <quaki@naver.com>
2023-11-15 16:38:19 +0900

    [Test]: 댓글 수정 및 삭제 테스트 작성

--- src/test/java/com/tenten/linkhub/domain/space/service/CommentServiceTest.java
+++ src/test/java/com/tenten/linkhub/domain/space/service/CommentServiceTest.java
@@ -13,7 +13,9 @@ import com.tenten.linkhub.domain.space.model.space.SpaceImage;
 import com.tenten.linkhub.domain.space.model.space.SpaceMember;
 import com.tenten.linkhub.domain.space.repository.comment.CommentJpaRepository;
 import com.tenten.linkhub.domain.space.repository.space.SpaceJpaRepository;
+import com.tenten.linkhub.domain.space.service.dto.CommentUpdateRequest;
 import com.tenten.linkhub.domain.space.service.dto.comment.RootCommentCreateRequest;
+import com.tenten.linkhub.global.exception.DataNotFoundException;
 import com.tenten.linkhub.global.exception.UnauthorizedAccessException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,6 +24,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -45,7 +49,10 @@ class CommentServiceTest {
 
     private Long setUpSpaceId1;
     private Long setUpSpaceId2;
-    private Long setUpMemberId;
+    private Long setUpMemberId1;
+    private Long setUpMemberId2;
+    private Long setUpCommentId1;
+    private Long setUpCommentId2;
 
     @BeforeEach
     void setUp() {
@@ -56,19 +63,19 @@ class CommentServiceTest {
     @DisplayName("유저는 스페이스에 댓글을 생성할 수 있다.")
     void createComment() {
         //given
-        RootCommentCreateRequest reqeust = new RootCommentCreateRequest(
+        RootCommentCreateRequest request = new RootCommentCreateRequest(
                 setUpSpaceId1,
-                setUpMemberId,
+                setUpMemberId1,
                 "첫번째 댓글");
 
         //when
-        Long savedCommentId = commentService.createComment(reqeust);
+        Long savedCommentId = commentService.createComment(request);
 
         //then
         Comment savedComment = commentJpaRepository.findById(savedCommentId).get();
 
         assertThat(savedComment.getContent()).isEqualTo("첫번째 댓글");
-        assertThat(savedComment.getMemberId()).isEqualTo(setUpMemberId);
+        assertThat(savedComment.getMemberId()).isEqualTo(setUpMemberId1);
         assertThat(savedComment.getSpace().getSpaceName()).isEqualTo("첫번째 스페이스");
     }
 
@@ -76,19 +83,123 @@ class CommentServiceTest {
     @DisplayName("유저가 댓글 여부를 false로 설정한 스페이스에 댓글을 생성하려고 할 경우 예외가 발생한다.")
     void createComment_UnauthorizedAccessException() {
         //given
-        RootCommentCreateRequest reqeust = new RootCommentCreateRequest(
+        RootCommentCreateRequest request = new RootCommentCreateRequest(
                 setUpSpaceId2,
-                setUpMemberId,
+                setUpMemberId1,
                 "첫번째 댓글");
 
         //when //then
-        assertThatThrownBy(() -> commentService.createComment(reqeust))
+        assertThatThrownBy(() -> commentService.createComment(request))
                 .isInstanceOf(UnauthorizedAccessException.class);
 
     }
 
+    @Test
+    @DisplayName("유저가 자신이 작성한 댓글을 수정한다.")
+    void updateComment() {
+        //given
+        CommentUpdateRequest request = new CommentUpdateRequest(
+                setUpSpaceId1,
+                setUpCommentId1,
+                setUpMemberId1,
+                "수정된 부모 댓글입니다.");
+
+        //when
+        Long updatedCommentId = commentService.updateComment(request);
+
+        // then
+        Comment updatedComment = commentJpaRepository.findById(updatedCommentId).get();
+
+        assertThat(updatedComment.getId()).isEqualTo(updatedCommentId);
+        assertThat(updatedComment.getContent()).isEqualTo("수정된 부모 댓글입니다.");
+        assertThat(updatedComment.getMemberId()).isEqualTo(setUpMemberId1);
+        assertThat(updatedComment.getSpace().getSpaceName()).isEqualTo("첫번째 스페이스");
+    }
+
+    @Test
+    @DisplayName("유저가 존재하지 않는 댓글을 수정하는데 실패한다.")
+    void updateComment_DataNotFoundException() {
+        //given
+        CommentUpdateRequest request = new CommentUpdateRequest(
+                setUpSpaceId1,
+                999L,
+                setUpMemberId1,
+                "수정된 부모 댓글입니다.");
+
+        //when //then
+        assertThatThrownBy(() -> commentService.updateComment(request))
+                .isInstanceOf(DataNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("유저가 타인의 댓글을 수정하는데 실패한다.")
+    void updateCommentOfOthers_UnauthorizedAccessException() {
+        //given
+        CommentUpdateRequest request = new CommentUpdateRequest(
+                setUpSpaceId1,
+                setUpCommentId1,
+                setUpMemberId2,
+                "수정된 부모 댓글입니다.");
+
+        //when //then
+        assertThatThrownBy(() -> commentService.updateComment(request))
+                .isInstanceOf(UnauthorizedAccessException.class);
+    }
+
+    @Test
+    @DisplayName("유저가 댓글을 달 수 없는 스페이스에서 댓글 수정을 실패한다.")
+    void updateCommentFromNotAllowedSpace_UnauthorizedAccessException() {
+        //given
+        CommentUpdateRequest request = new CommentUpdateRequest(
+                setUpSpaceId2,
+                setUpCommentId2,
+                setUpMemberId1,
+                "수정된 부모 댓글입니다.");
+
+        //when //then
+        assertThatThrownBy(() -> commentService.updateComment(request))
+                .isInstanceOf(UnauthorizedAccessException.class);
+    }
+
+    @Test
+    @DisplayName("유저가 자신이 단 댓글을 삭제하는데 성공한다.")
+    void deleteComment() {
+        //given //when
+        Long deletedCommentId = commentService.deleteComment(setUpSpaceId1, setUpCommentId1, setUpMemberId1);
+
+        // then
+        Optional<Comment> deletedComment = commentJpaRepository.findById(deletedCommentId);
+
+        assertThat(deletedCommentId).isEqualTo(setUpCommentId1);
+        assertThat(deletedComment).isEmpty();
+    }
+
+    @Test
+    @DisplayName("유저가 존재하지 않는 댓글을 삭제하는데 실패한다.")
+    void deleteComment_DataNotFoundException() {
+        //given //when //then
+        assertThatThrownBy(() -> commentService.deleteComment(setUpSpaceId1, 999L, setUpMemberId1))
+                .isInstanceOf(DataNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("유저가 타인이 작성한 댓글을 삭제하는데 실패한다.")
+    void deleteCommentOfOthers_UnauthorizedException() {
+        //given //when //then
+        assertThatThrownBy(() -> commentService.deleteComment(setUpSpaceId1, setUpCommentId1, setUpMemberId2))
+                .isInstanceOf(UnauthorizedAccessException.class);
+    }
+
+    @Test
+    @DisplayName("유저가 댓글 기능이 비활성화된 스페이스 내 댓글 삭제를 실패한다.")
+    void deleteCommentFromNotAllowedSpace_UnauthorizedException() {
+        //given //when //then
+        assertThatThrownBy(() -> commentService.deleteComment(setUpSpaceId2, setUpCommentId2, setUpMemberId1))
+                .isInstanceOf(UnauthorizedAccessException.class);
+    }
+
     private void setUpTestData(){
-        Member member = new Member(
+        Member member1 = new Member(
                 "testSocialId",
                 Provider.kakao,
                 com.tenten.linkhub.domain.member.model.Role.USER,
@@ -100,15 +211,28 @@ class CommentServiceTest {
                 new FavoriteCategory(Category.KNOWLEDGE_ISSUE_CAREER)
         );
 
-        setUpMemberId = memberJpaRepository.save(member).getId();
+        Member member2 = new Member(
+                "testSocialId2",
+                Provider.kakao,
+                com.tenten.linkhub.domain.member.model.Role.USER,
+                "잠자는 사자의 포효",
+                "테스트용 소개글",
+                "abcd@gmail.com",
+                true,
+                new ProfileImage("https://testprofileimage", "테스트용 멤버 프로필 이미지"),
+                new FavoriteCategory(Category.KNOWLEDGE_ISSUE_CAREER)
+        );
+
+        setUpMemberId1 = memberJpaRepository.save(member1).getId();
+        setUpMemberId2 = memberJpaRepository.save(member2).getId();
 
         Space space1 = new Space(
-                setUpMemberId,
+                setUpMemberId1,
                 "첫번째 스페이스",
                 "첫번째 스페이스 소개글",
                 Category.KNOWLEDGE_ISSUE_CAREER,
                 new SpaceImage("https://testimage1", "테스트 이미지1"),
-                new SpaceMember(setUpMemberId, Role.OWNER),
+                new SpaceMember(setUpMemberId1, Role.OWNER),
                 true,
                 true,
                 true,
@@ -116,12 +240,12 @@ class CommentServiceTest {
         );
 
         Space space2 = new Space(
-                setUpMemberId,
+                setUpMemberId1,
                 "두번째 스페이스",
                 "두번째 스페이스 소개글",
                 Category.ETC,
                 new SpaceImage( "https://testimage2", "테스트 이미지2"),
-                new SpaceMember(setUpMemberId, Role.OWNER),
+                new SpaceMember(setUpMemberId1, Role.OWNER),
                 true,
                 false,
                 true,
@@ -130,6 +254,26 @@ class CommentServiceTest {
 
         setUpSpaceId1 = spaceJpaRepository.save(space1).getId();
         setUpSpaceId2 = spaceJpaRepository.save(space2).getId();
+
+        Comment comment1 = new Comment(
+                null,
+                null,
+                "부모 댓글입니다.",
+                member1.getId(),
+                space1
+        );
+
+        Comment comment2 = new Comment(
+                null,
+                null,
+                "부모 댓글입니다.",
+                member1.getId(),
+                space2
+        );
+
+        setUpCommentId1 = commentJpaRepository.save(comment1).getId();
+        setUpCommentId2 = commentJpaRepository.save(comment2).getId();
+
     }
 
 }
