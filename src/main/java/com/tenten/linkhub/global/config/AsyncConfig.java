8672ccbb707e9096a0864f27cb5d42a78f37c5b8
KimMinhee <99alsgmlalsl@naver.com>
2023-11-18 01:53:45 +0900

    feat: Async Config 파일 설정

---
+++ src/main/java/com/tenten/linkhub/global/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.tenten.linkhub.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("linkhub-async");
+        executor.initialize();
+        return executor;
+    }
+}
