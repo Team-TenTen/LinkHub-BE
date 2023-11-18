24bc716ce70b6024e7fdc3777747943130bc2dd3
young970 <yyww9@naver.com>
2023-11-19 02:13:55 +0900

    [Chore]: 중복된 `@EnableAsync` 제거.

--- src/test/java/com/tenten/linkhub/domain/space/service/FavoriteServiceTest.java
+++ src/test/java/com/tenten/linkhub/domain/space/service/FavoriteServiceTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
