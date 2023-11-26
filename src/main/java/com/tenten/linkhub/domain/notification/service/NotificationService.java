224cf786eb667a02227676d51022d1f1ba5d1b89
KimMinhee <99alsgmlalsl@naver.com>
2023-11-27 00:00:55 +0900

    [Fix]: createNotification 메소드에 @Transactional 추가

--- src/main/java/com/tenten/linkhub/domain/notification/service/NotificationService.java
+++ src/main/java/com/tenten/linkhub/domain/notification/service/NotificationService.java
@@ -4,10 +4,10 @@ import com.tenten.linkhub.domain.notification.model.Notification;
 import com.tenten.linkhub.domain.notification.model.NotificationType;
 import com.tenten.linkhub.domain.notification.repository.NotificationRepository;
 import com.tenten.linkhub.domain.notification.repository.dto.SpaceInvitationNotificationGetDto;
+import com.tenten.linkhub.domain.notification.service.dto.NotificationCreateRequest;
 import com.tenten.linkhub.domain.notification.service.dto.SpaceInviteNotificationGetRequest;
 import com.tenten.linkhub.domain.notification.service.dto.SpaceInviteNotificationGetResponses;
 import com.tenten.linkhub.domain.notification.service.mapper.NotificationMapper;
-import com.tenten.linkhub.domain.notification.service.dto.NotificationCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -30,8 +30,11 @@ public class NotificationService {
 
     public SpaceInviteNotificationGetResponses getSpaceInvitations(SpaceInviteNotificationGetRequest request) {
         Slice<SpaceInvitationNotificationGetDto> notificationGetDtos = notificationRepository.getInviteNotifications(notificationMapper.toQueryCondition(request));
+
         return SpaceInviteNotificationGetResponses.from(notificationGetDtos);
     }
+
+    @Transactional
     public Long createNotification(NotificationCreateRequest request) {
 
         Notification notification = new Notification(
