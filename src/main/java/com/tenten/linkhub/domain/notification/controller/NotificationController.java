f2e31001c35db93f69253efd48efb728eebe026e
KimMinhee <99alsgmlalsl@naver.com>
2023-11-26 23:30:40 +0900

    docs: swagger 코드 추가

--- src/main/java/com/tenten/linkhub/domain/notification/controller/NotificationController.java
+++ src/main/java/com/tenten/linkhub/domain/notification/controller/NotificationController.java
@@ -7,6 +7,8 @@ import com.tenten.linkhub.domain.notification.controller.mapper.NotificationApiM
 import com.tenten.linkhub.domain.notification.service.NotificationService;
 import com.tenten.linkhub.domain.notification.service.dto.SpaceInviteNotificationGetRequest;
 import com.tenten.linkhub.domain.notification.service.dto.SpaceInviteNotificationGetResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -28,6 +30,11 @@ public class NotificationController {
     /**
      * 스페이스 초대 알림 조회 API
      */
+    @Operation(
+            summary = "스페이스 초대 알림 조회 API ", description = "[JWT 필요] - pageNumber, pageSize를 받아 스페이스 초대 알림 내역을 조회합니다. ",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "초대 알림 조회가 성공적으로 완료되었습니다.")
+            })
     @GetMapping(value = "/invitations")
     public ResponseEntity<SpaceInviteNotificationGetApiResponses> getInviteNotifications(
             @ModelAttribute SpaceInviteNotificationGetApiRequest request,
