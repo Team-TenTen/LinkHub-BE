03578f8b0d5cf1750ca772e361c68ac62558b814
KimMinhee <99alsgmlalsl@naver.com>
2023-11-27 20:42:47 +0900

    [Fix]: 스웨거 예시 코드 수정

--- src/main/java/com/tenten/linkhub/domain/notification/controller/dto/SpaceInviteNotificationGetApiRequest.java
+++ src/main/java/com/tenten/linkhub/domain/notification/controller/dto/SpaceInviteNotificationGetApiRequest.java
@@ -3,7 +3,7 @@ package com.tenten.linkhub.domain.notification.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record SpaceInviteNotificationGetApiRequest(
-        @Schema(title = "페이지 번호", example = "1")
+        @Schema(title = "페이지 번호", example = "0")
         Integer pageNumber,
 
         @Schema(title = "페이지 크기", example = "10")
