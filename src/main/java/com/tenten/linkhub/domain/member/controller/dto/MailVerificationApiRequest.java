00aa896bcb3a340a2f51b542966c50432b1cf2ee
KimMinhee <99alsgmlalsl@naver.com>
2023-12-01 17:52:32 +0900

    [Fix]: 이메일 인증 API Request 내 validation 메시지 추가

--- src/main/java/com/tenten/linkhub/domain/member/controller/dto/MailVerificationApiRequest.java
+++ src/main/java/com/tenten/linkhub/domain/member/controller/dto/MailVerificationApiRequest.java
@@ -1,14 +1,16 @@
 package com.tenten.linkhub.domain.member.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record MailVerificationApiRequest(
-        @NotBlank
-        @Email
+        @Schema(title = "이메일", example = "fordevmin@gmail.com")
+        @NotBlank(message = "메일은 빈 값이 들어올 수 없습니다.")
+        @Email(message = "유효한 이메일 형식이 아닙니다.")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "인증번호는 빈 값이 들어올 수 없습니다.")
         String code
 ) {
 }
